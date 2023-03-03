374bda502cec0a8285897570e76612ed48242dee
Sungwoo <sungwoo.cho.dev@gmail.com>
2023-03-03 14:55:49 +0900

    auth js experimental

---
+++ apps/nextjs/components/LoginButton.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+export default function LoginButton() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button type="button" onClick={() => signOut()}>
+          Sign out
+        </button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      Not signed in <br />
+      <button type="button" onClick={() => signIn()}>
+        Sign in
+      </button>
+    </>
+  );
+}
--- apps/nextjs/pages/index.tsx
+++ apps/nextjs/pages/index.tsx
@@ -1,4 +1,10 @@
 import React from "react";
 import { Button } from "@nighttrax/components/button";
+import LoginButton from "../components/LoginButton";
 
-export default () => <Button />;
+export default () => (
+  <>
+    <Button />
+    <LoginButton />
+  </>
+);
